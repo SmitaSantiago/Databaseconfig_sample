c95418944f042cbf57413f3a166d7c4f87f06c8b
SmitaSantiago <70471613+SmitaSantiago@users.noreply.github.com>
2021-09-01 07:02:11 +0530

    Add files via upload

--- C-35/asynchronousBallMovement-master/sketch.js
+++ C-35/asynchronousBallMovement-master/sketch.js
@@ -22,23 +22,27 @@ hypnoticballPosition.on("value",readPosition, showError)
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+
+    database.ref("ball/positions").set({
+        'x' : position.x + x,
+        'y' : position.y + y
+    })
+    
 }
 
 function readPosition(data){
